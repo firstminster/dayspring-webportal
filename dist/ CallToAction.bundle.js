a2a16136bd0796f8ffe5e64a1077e6c0e0cf75c6
Chinecherem Harry <chinecherempharry@gmail.com>
2022-11-12 15:00:10 +0100

    working on the student review section

---
+++ dist/ CallToAction.bundle.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkdayspring_webportal"] = self["webpackChunkdayspring_webportal"] || []).push([[" CallToAction"],{
+
+/***/ "./src/components/home/CallToAction.jsx":
+/*!**********************************************!*\
+  !*** ./src/components/home/CallToAction.jsx ***!
+  \**********************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var react_icons_fa__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! react-icons/fa */ \"./node_modules/react-icons/fa/index.esm.js\");\n/* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react-router-dom */ \"./node_modules/react-router-dom/dist/index.js\");\n/* harmony import */ var _assets_importAssets__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../assets/importAssets */ \"./src/assets/importAssets.js\");\n\n\n\n\n\nvar CallToAction = function CallToAction() {\n  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"div\", {\n    className: \"absolute top-[390rem] sm:top-[350rem] md:top-[330rem] lg:top-[283rem] inset-x-0\"\n  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"div\", {\n    className: \"container mx-auto flex justify-center items-center\"\n  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"div\", {\n    className: \"absolute text-white flex flex-col justify-center items-center py-[46px]\"\n  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"h2\", {\n    className: \"text-3xl lg:text-4xl w-[440px] lg:w-[803px] text-center font-semibold\"\n  }, \"Provide standard education for your kids and watch them become the successful leaders they were born to be.\"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_2__.NavLink, {\n    to: \"#\",\n    className: \"flex items-center justify-center w-[281px] h-[49px] mt-[22px] bg-white text-blue-primary text-sm xl:text-lg rounded-full hover:bg-medium-orchid hover:text-white transition-all duration-300 ease-out \"\n  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"span\", {\n    className: \"mr-[11px]\"\n  }, \"Start Now\"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(react_icons_fa__WEBPACK_IMPORTED_MODULE_3__.FaArrowRight, {\n    className: \"w-[13.54px] h-[11.89px]\"\n  }))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"img\", {\n    src: _assets_importAssets__WEBPACK_IMPORTED_MODULE_1__.patternDoodleBlackImg,\n    alt: \"pattern\",\n    className: \"bg-blue-primary sm:rounded-2xl w-full h-[311px] lg:w-[1176px] lg:h-[298px] object-cover\"\n  })));\n};\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (CallToAction);\n\n//# sourceURL=webpack://dayspring-webportal/./src/components/home/CallToAction.jsx?");
+
+/***/ })
+
+}]);
---
+++ dist/Metrics.bundle.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkdayspring_webportal"] = self["webpackChunkdayspring_webportal"] || []).push([["Metrics"],{
+
+/***/ "./src/components/home/Metrics.jsx":
+/*!*****************************************!*\
+  !*** ./src/components/home/Metrics.jsx ***!
+  \*****************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _assets_importAssets__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../assets/importAssets */ \"./src/assets/importAssets.js\");\n\n\n\nvar Metrics = function Metrics() {\n  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"div\", {\n    className: \"absolute top-[173rem] sm:top-[160rem] lg:top-[135rem] inset-x-0 \"\n  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"div\", {\n    className: \"container mx-auto absolute inset-0 flex flex-col lg:flex-row justify-center items-center text-white space-y-[41px] lg:space-y-0 lg:space-x-[169px] \"\n  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"div\", {\n    className: \"text-center\"\n  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"h2\", {\n    className: \"text-[40px] font-semibold -mb-3\"\n  }, \"5000+\"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"p\", {\n    className: \"text-lg font-light \"\n  }, \"Alumnis\")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"div\", {\n    className: \"text-center\"\n  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"h2\", {\n    className: \"text-[40px] font-semibold -mb-3\"\n  }, \"30+\"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"p\", {\n    className: \"text-lg font-light\"\n  }, \"Teachers\")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"div\", {\n    className: \"text-center\"\n  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"h2\", {\n    className: \"text-[40px] font-semibold -mb-3\"\n  }, \"90%\"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"p\", {\n    className: \"text-lg font-light\"\n  }, \"Success Rate\"))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"img\", {\n    src: _assets_importAssets__WEBPACK_IMPORTED_MODULE_1__.metricsImg,\n    alt: \"metrics-bg\",\n    className: \"hidden lg:block h-[163px] w-full object-cover\"\n  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"img\", {\n    src: _assets_importAssets__WEBPACK_IMPORTED_MODULE_1__.patternDoodleWhiteMetricsMobileImg,\n    alt: \"metrics-bg\",\n    className: \"lg:hidden h-[462px] w-full object-cover bg-blue-primary\"\n  }));\n};\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (Metrics);\n\n//# sourceURL=webpack://dayspring-webportal/./src/components/home/Metrics.jsx?");
+
+/***/ })
+
+}]);
